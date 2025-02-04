b31cf70133de83e8ad69c214b27a8d17ed3e2d2b
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2025-02-04 10:02:09 +0100

    Creado metodo getAsignaturas() en FirestoreManager.kt

--- app/src/main/java/com/munozcastrovirginia/proyectoapi/data/FirestoreManager.kt
+++ app/src/main/java/com/munozcastrovirginia/proyectoapi/data/FirestoreManager.kt
@@ -2,8 +2,38 @@ package com.munozcastrovirginia.proyectoapi.data
 
 import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.snapshots
+import com.google.firebase.firestore.toObject
+import com.munozcastrovirginia.proyectoapi.model.Asignatura
+import com.munozcastrovirginia.proyectoapi.model.AsignaturaDB
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class FirestoreManager(auth: AuthManager, context: Context) {
     private val firestore = FirebaseFirestore.getInstance()
     private val userId = auth.getCurrentUser()?.uid
+
+    companion object {
+        const val ASIGNATURA_COLLECTION = "Asignaturas"
+    }
+
+    fun getAsignaturas(): Flow<List<Asignatura>> {
+        return firestore.collection(ASIGNATURA_COLLECTION)
+            .whereEqualTo("userId", userId)
+            .snapshots()
+            .map { qs ->
+                qs.documents.mapNotNull { ds ->
+                    ds.toObject(AsignaturaDB::class.java)?.let { asignaturaDB ->
+                        Asignatura(
+                            id = ds.id,
+                            userId = asignaturaDB.userId,
+                            codigo = asignaturaDB.codigo,
+                            nombre = asignaturaDB.nombre,
+                            descripcion = asignaturaDB.descripcion,
+                            horas = asignaturaDB.horas
+                        )
+                    }
+                }
+            }
+    }
 }
