301ac1d111360ac092d281a6ae4296776c58dc07
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2025-02-05 10:01:46 +0100

    Crea la asignatura al darle al boton de añadir

--- app/src/main/java/com/munozcastrovirginia/proyectoapi/model/AsignaturaDB.kt
+++ app/src/main/java/com/munozcastrovirginia/proyectoapi/model/AsignaturaDB.kt
@@ -1,9 +1,9 @@
 package com.munozcastrovirginia.proyectoapi.model
 
 data class AsignaturaDB(
-    val userId: String,
-    val codigo: String,
-    val nombre: String,
-    val descripcion: String,
-    val horas: Int
+    val userId: String = "",
+    val codigo: String = "",
+    val nombre: String = "",
+    val descripcion: String = "",
+    val horas: Int = 0
 )
